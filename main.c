081170f0bdc057aad6a94ad352e79dcd6257f57c
CSCI3150 <1155079818@link.cuhk.edu.hk>
2019-05-10 12:32:57 +0800

    finish tcp

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <sys/types.h>          /* See NOTES */
+#include <sys/socket.h>
+#include <stdio.h>
+#include "es_timer.h"
+
+int main(int argc, char** argv){
+	ES_Timer timer;
+	printf("%s\n", "i love world");
+}
