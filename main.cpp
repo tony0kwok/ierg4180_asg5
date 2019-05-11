aa4bcfef4367ef110745824c95a243bfee88b99e
CSCI3150 <1155079818@link.cuhk.edu.hk>
2019-05-11 13:35:12 +0800

    finish tcp

--- main.cpp
+++ main.cpp
@@ -50,14 +50,14 @@ int setting(int argc, char** argv){
 
 	stat_ms = 500;
 	memset(stat, '\0', sizeof(stat));
-	strcpy(stat, "Hello World");
+	strcpy(stat, "Accepting connection...");
 
 	bsize = 1000;
 	bufsize = bsize;
 
 	pktrate = 1000;
 
-	num = 9999;
+	num = 99999;
 
 	const char *optstring = "srp:k:b:u";
     int c;
@@ -118,10 +118,48 @@ void msleep(int ms){
 	return;
 }
 
+char *send_stat_cal(int usec_time, int package_no){
+	char *output = (char *) malloc(100);
+	sprintf(output, "Rate[%.2lfMbps]", (double)(package_no+1)*bsize*1000/usec_time);
+	return output;
+}
+
+char *recv_stat_cal(int usec_time, int package_no){
+	static int recieved = 0, max = 0;
+	static int previous_time = 0, pretrans_time = 0;
+	static int deltasum = 0;
+	double jitter = 0;
+	if (package_no>max)
+		max = package_no;
+	recieved++;
+	int lost = package_no+1 - recieved;
+	double lostrate = (double)lost/recieved;
+	double rate = (double)bsize*recieved*1000/usec_time;
+	
+	if (recieved>=2){
+		if (int delta = usec_time - previous_time - pretrans_time>0)
+			deltasum += delta;
+		else
+			deltasum -= delta;
+	}
+	jitter = deltasum/(float)recieved;
+	pretrans_time = usec_time - previous_time;
+	previous_time = usec_time;
+
+	char *output = (char *)malloc(100);
+	sprintf(output, "Pkts [%d] Lost [%d, %.2lf%%] Rate [%.2lfMbps] Jitter [%.2lfus]", recieved, lost, lostrate, rate, jitter);
+	return output;
+}
+
 void* threadfunc(void* data){
+	ES_Timer timer;
+	timer.Start();
+	char *elapsedtime = (char *)malloc(100);
 	while(1){
+		sprintf(elapsedtime, "Elapsed [%.2lfs] ",(double)(timer.ElapseduSec())/1000000);
 		msleep(stat_ms);
-		printf("%s\n", stat);
+		strcat(elapsedtime, stat);
+		printf("%s\n", elapsedtime);
 	}
 	return NULL;
 }
@@ -149,12 +187,15 @@ void tcp_server(){
     bind(sockfd,(struct sockaddr *)&serverInfo,sizeof(serverInfo));
     listen(sockfd,5);
     forClientSockfd = accept(sockfd,(struct sockaddr *)&clientInfo, &addrlen);
+    strcpy(stat, "Packet transmitting..");
     
     int recvb = 0;
     int temsum = 0;
 
     int count = 0;
 
+    ES_Timer timer;
+    timer.Start();
     while(1){
     	temsum = 0;
     	memset(outputBuffer, '\0', bsize);
@@ -165,14 +206,13 @@ void tcp_server(){
     			printf("Packages all recieved.\n");
     			exit(0);
     		}
-    		printf("BUF SIZE=%d\n", recvb);
+    		//printf("BUF SIZE=%d\n", recvb);
     		strcpy(outputBuffer+temsum, inputBuffer);
     		//printf("%s\n", outputBuffer);
     		temsum += recvb;
     	}
-    	printf("This is package %d", decode_header(outputBuffer));
-        count++;
-        sprintf(stat, "recieved %d package, package size = %d", count, temsum);
+    	//printf("This is package %d", decode_header(outputBuffer));
+        sprintf(stat, "%s\n", recv_stat_cal(timer.ElapseduSec(),decode_header(outputBuffer)));
     }
 }
 
@@ -203,22 +243,122 @@ void tcp_client(){
     int sendb;
     int temsum = 0;
 
+    ES_Timer timer;
+    timer.Start();
+
     for(int i = 0; i<num; i++){
     	temsum = 0;
     	memset(message,'\n',sizeof(message));
     	encode_header(message, i);
-    	printf("%d\n", decode_header(message));
 
     	//keep sending before put all data of a package into buf
     	while(bsize>temsum){
-	    	msleep(1000/(pktrate/bufsize));
-	    	printf("%s\n", message+temsum);
+    		if(pktrate>0)
+	    		msleep(1000/(pktrate/bufsize));
 	    	sendb = send(sockfd, message+temsum, bufsize>bsize-temsum ? bsize-temsum: bufsize, 0);
 	    	temsum += sendb;
-	    	printf("temsum = %d\n", temsum);
 	    }
-    	count++;
-    	sprintf(stat, "sent %d package, temsum = %d", count, temsum);
+    	sprintf(stat, "%s\n", send_stat_cal(timer.ElapseduSec(), i));
+    }
+    msleep(stat_ms);
+    close(sockfd);
+}
+
+void udp_server(){
+	char *inputBuffer = (char *) malloc(sizeof(char)*bufsize);
+	char *outputBuffer = (char *) malloc(sizeof(char)*bsize);
+	memset(inputBuffer, '\0', bufsize);
+	memset(outputBuffer, '\0', bsize);
+    int sockfd = 0,forClientSockfd = 0;
+    sockfd = socket(AF_INET , SOCK_STREAM , 0);
+
+    if (sockfd == -1){
+        printf("Fail to create a socket.");
+    }
+
+    //socket的連線
+    struct sockaddr_in serverInfo,clientInfo;
+    socklen_t addrlen = sizeof(clientInfo);
+    bzero(&serverInfo,sizeof(serverInfo));
+
+    serverInfo.sin_family = PF_INET;
+    serverInfo.sin_addr.s_addr = INADDR_ANY;
+    serverInfo.sin_port = htons(port);
+    bind(sockfd,(struct sockaddr *)&serverInfo,sizeof(serverInfo));
+    listen(sockfd,5);
+    forClientSockfd = accept(sockfd,(struct sockaddr *)&clientInfo, &addrlen);
+    strcpy(stat, "Packet transmitting..");
+    
+    int recvb = 0;
+    int temsum = 0;
+
+    int count = 0;
+
+    ES_Timer timer;
+    timer.Start();
+    while(1){
+    	temsum = 0;
+    	memset(outputBuffer, '\0', bsize);
+    	while(bsize>temsum){
+        	recvb = recv(forClientSockfd,inputBuffer,bufsize,0);
+    		if (recvb==0)
+    		{
+    			printf("Packages all recieved.\n");
+    			exit(0);
+    		}
+    		//printf("BUF SIZE=%d\n", recvb);
+    		strcpy(outputBuffer+temsum, inputBuffer);
+    		//printf("%s\n", outputBuffer);
+    		temsum += recvb;
+    	}
+    	//printf("This is package %d", decode_header(outputBuffer));
+        sprintf(stat, "%s\n", recv_stat_cal(timer.ElapseduSec(),decode_header(outputBuffer)));
+    }
+}
+
+void udp_client(){
+	//set the package size
+	char *message = (char *) malloc(sizeof(char)*bsize);
+	memset(message, '\0', sizeof(char)*bsize);
+
+
+	int sockfd = socket(domain, type, protocol);
+
+	if (sockfd == -1){
+        printf("Fail to create a socket.");
+    }
+
+    struct sockaddr_in info;	//set address info
+    bzero(&info,sizeof(info));
+    info.sin_family = domain;
+    info.sin_addr.s_addr = inet_addr(address);
+    info.sin_port = htons(port);
+    
+    int err = connect(sockfd,(struct sockaddr *)&info,sizeof(info));
+    if(err==-1){
+        printf("Connection error");
+    }
+
+    int count = 0;
+    int sendb;
+    int temsum = 0;
+
+    ES_Timer timer;
+    timer.Start();
+
+    for(int i = 0; i<num; i++){
+    	temsum = 0;
+    	memset(message,'\n',sizeof(message));
+    	encode_header(message, i);
+
+    	//keep sending before put all data of a package into buf
+    	while(bsize>temsum){
+    		if(pktrate>0)
+	    		msleep(1000/(pktrate/bufsize));
+	    	sendb = send(sockfd, message+temsum, bufsize>bsize-temsum ? bsize-temsum: bufsize, 0);
+	    	temsum += sendb;
+	    }
+    	sprintf(stat, "%s\n", send_stat_cal(timer.ElapseduSec(), i));
     }
     msleep(stat_ms);
     close(sockfd);
@@ -236,7 +376,5 @@ int main(int argc, char** argv){
 		tcp_client();
 	}
 	
-
-	ES_Timer timer;
-	printf("%s\n", "i love world");
+	printf("%s\n", "program ends\n");
 }
