cb24bfb6aaefe9edae62b10158d811e19120e3be
CSCI3150 <1155079818@link.cuhk.edu.hk>
2019-05-11 15:13:27 +0800

    finish udp

--- main.cpp
+++ main.cpp
@@ -28,6 +28,8 @@ int pktrate;
 
 int num;
 
+int proto;
+
 void encode_header(char *buffer, int number){
 	//support maximum sequence number 10000 packages
 	sprintf(buffer, "%d%d%d%d", number/1000, (number%1000)/100, (number%100)/10, number%10);
@@ -41,7 +43,7 @@ int setting(int argc, char** argv){
 	//default setting
 	mode = 0; //0=server,1=client
 	domain = AF_INET;	//AF_INET6
-	type = SOCK_STREAM; //SOCK_DGRAM
+	type = SOCK_DGRAM; //SOCK_DGRAM
 	protocol = 0;
 	memset(address, '\0', sizeof(address));
 	char tony[] = "127.0.0.1";
@@ -59,7 +61,7 @@ int setting(int argc, char** argv){
 
 	num = 99999;
 
-	const char *optstring = "srp:k:b:u";
+	const char *optstring = "srp:k:b:u:";
     int c;
     struct option opts[] = {
         {"send", 0, NULL, 's'},
@@ -70,10 +72,20 @@ int setting(int argc, char** argv){
         {"rbufsize", 1, NULL, 'b'},
         {"sbufsize", 1, NULL, 'u'},
         {"pktrate", 1, NULL, 't'},
-        {"pktnum", 1, NULL , 'n'}
+        {"pktnum", 1, NULL , 'n'},
+        {"proto", 1, NULL, 'o'}
     };
     while((c = getopt_long(argc, argv, optstring, opts, NULL)) != -1) {
         switch(c) {
+        	case 'o':
+        		if (strcmp(optarg,"tcp")==0)
+        		{
+        			type = SOCK_STREAM;
+        		}
+        		else{
+        			type = SOCK_DGRAM;
+        		}
+        		break;
         	case 'n':
         		num = atoi(optarg);
         		break;
@@ -170,50 +182,81 @@ void tcp_server(){
 	memset(inputBuffer, '\0', bufsize);
 	memset(outputBuffer, '\0', bsize);
     int sockfd = 0,forClientSockfd = 0;
-    sockfd = socket(AF_INET , SOCK_STREAM , 0);
+    sockfd = socket(AF_INET , type , 0);
 
     if (sockfd == -1){
         printf("Fail to create a socket.");
     }
 
-    //socket的連線
+    //sever socket的連線
     struct sockaddr_in serverInfo,clientInfo;
     socklen_t addrlen = sizeof(clientInfo);
     bzero(&serverInfo,sizeof(serverInfo));
+    bzero(&clientInfo,sizeof(clientInfo));
 
     serverInfo.sin_family = PF_INET;
     serverInfo.sin_addr.s_addr = INADDR_ANY;
     serverInfo.sin_port = htons(port);
     bind(sockfd,(struct sockaddr *)&serverInfo,sizeof(serverInfo));
-    listen(sockfd,5);
-    forClientSockfd = accept(sockfd,(struct sockaddr *)&clientInfo, &addrlen);
-    strcpy(stat, "Packet transmitting..");
-    
-    int recvb = 0;
-    int temsum = 0;
 
-    int count = 0;
+    //tcp
+    if(type == SOCK_STREAM){
+	    listen(sockfd,5);
+	    forClientSockfd = accept(sockfd,(struct sockaddr *)&clientInfo, &addrlen);
+	    strcpy(stat, "Packet transmitting..");
+	    
+	    int recvb = 0;
+	    int temsum = 0;
 
-    ES_Timer timer;
-    timer.Start();
-    while(1){
-    	temsum = 0;
-    	memset(outputBuffer, '\0', bsize);
-    	while(bsize>temsum){
-        	recvb = recv(forClientSockfd,inputBuffer,bufsize,0);
-    		if (recvb==0)
-    		{
-    			printf("Packages all recieved.\n");
-    			exit(0);
-    		}
-    		//printf("BUF SIZE=%d\n", recvb);
-    		strcpy(outputBuffer+temsum, inputBuffer);
-    		//printf("%s\n", outputBuffer);
-    		temsum += recvb;
-    	}
-    	//printf("This is package %d", decode_header(outputBuffer));
-        sprintf(stat, "%s\n", recv_stat_cal(timer.ElapseduSec(),decode_header(outputBuffer)));
-    }
+	    int count = 0;
+
+	    ES_Timer timer;
+	    timer.Start();
+	    while(1){
+	    	temsum = 0;
+	    	memset(outputBuffer, '\0', bsize);
+	    	while(bsize>temsum){
+	        	recvb = recv(forClientSockfd,inputBuffer,bufsize,0);
+	    		if (recvb==0)
+	    		{
+	    			printf("Packages all recieved.\n");
+	    			exit(0);
+	    		}
+	    		//printf("BUF SIZE=%d\n", recvb);
+	    		strcpy(outputBuffer+temsum, inputBuffer);
+	    		//printf("%s\n", outputBuffer);
+	    		temsum += recvb;
+	    	}
+	    	//printf("This is package %d", decode_header(outputBuffer));
+	        sprintf(stat, "%s\n", recv_stat_cal(timer.ElapseduSec(),decode_header(outputBuffer)));
+	    }
+	}
+
+	if (type == SOCK_DGRAM)
+	{
+		ES_Timer timer;
+	    timer.Start();
+		int temsum;
+		int nbytes;
+		strcpy(stat, "Accepting Datagram..");
+		while(1){
+			temsum = 0;
+			memset(outputBuffer, '\0', bsize);
+			while(bsize>temsum){
+				if (nbytes = recvfrom(sockfd, inputBuffer, bufsize, 0, (struct sockaddr*)&clientInfo, &addrlen) < 0) {
+	                        perror ("could not read datagram!!");
+	                        continue;
+	                }
+	            strcpy(outputBuffer+temsum, inputBuffer);
+	            temsum+=nbytes;
+
+	            //force the loop end
+	            temsum=bsize;
+
+            }
+            sprintf(stat, "%s\n", recv_stat_cal(timer.ElapseduSec(),decode_header(outputBuffer))); 
+		}
+	}
 }
 
 void tcp_client(){
@@ -228,40 +271,77 @@ void tcp_client(){
         printf("Fail to create a socket.");
     }
 
-    struct sockaddr_in info;	//set address info
+    struct sockaddr_in info, myaddr;	//set address info
     bzero(&info,sizeof(info));
     info.sin_family = domain;
     info.sin_addr.s_addr = inet_addr(address);
     info.sin_port = htons(port);
-    
-    int err = connect(sockfd,(struct sockaddr *)&info,sizeof(info));
-    if(err==-1){
-        printf("Connection error");
-    }
 
-    int count = 0;
-    int sendb;
-    int temsum = 0;
+    //myaddr for returning
+    bzero((char *)&myaddr, sizeof(myaddr));
+    myaddr.sin_family = AF_INET;
+    myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    myaddr.sin_port = htons(0);
 
-    ES_Timer timer;
-    timer.Start();
-
-    for(int i = 0; i<num; i++){
-    	temsum = 0;
-    	memset(message,'\n',sizeof(message));
-    	encode_header(message, i);
-
-    	//keep sending before put all data of a package into buf
-    	while(bsize>temsum){
-    		if(pktrate>0)
-	    		msleep(1000/(pktrate/bufsize));
-	    	sendb = send(sockfd, message+temsum, bufsize>bsize-temsum ? bsize-temsum: bufsize, 0);
-	    	temsum += sendb;
+    if(type == SOCK_STREAM){
+	    int err = connect(sockfd,(struct sockaddr *)&info,sizeof(info));
+	    if(err==-1){
+	        printf("Connection error");
 	    }
-    	sprintf(stat, "%s\n", send_stat_cal(timer.ElapseduSec(), i));
-    }
-    msleep(stat_ms);
-    close(sockfd);
+
+	    int count = 0;
+	    int sendb;
+	    int temsum = 0;
+
+	    ES_Timer timer;
+	    timer.Start();
+
+	    for(int i = 0; i<num; i++){
+	    	temsum = 0;
+	    	memset(message,'\n',sizeof(message));
+	    	encode_header(message, i);
+
+	    	//keep sending before put all data of a package into buf
+	    	while(bsize>temsum){
+	    		if(pktrate>0)
+		    		msleep(1000/(pktrate/bufsize));
+		    	sendb = send(sockfd, message+temsum, bufsize>bsize-temsum ? bsize-temsum: bufsize, 0);
+		    	temsum += sendb;
+		    }
+	    	sprintf(stat, "%s\n", send_stat_cal(timer.ElapseduSec(), i));
+	    }
+	    msleep(stat_ms);
+	    close(sockfd);	
+	}
+
+	if (type==SOCK_DGRAM)
+	{
+		if (bind(sockfd, (struct sockaddr *)&myaddr,
+                            sizeof(myaddr)) <0) {
+            perror("bind failed!");
+            exit(1);
+    	}
+		int temsum;
+		int sendb;
+		ES_Timer timer;
+	    timer.Start();
+
+	    for(int i = 0; i<num; i++){
+	    	temsum = 0;
+	    	memset(message,'\n',sizeof(message));
+	    	encode_header(message, i);
+
+	    	//keep sending before put all data of a package into buf
+	    	while(bsize>temsum){
+	    		if(pktrate>0)
+		    		msleep(1000/(pktrate/bufsize));
+		    	sendb = sendto(sockfd, message+temsum, bufsize>bsize-temsum ? bsize-temsum: bufsize, 0, (struct sockaddr *)&info,sizeof(info));
+		    	temsum += sendb;
+		    	printf("%d\n", temsum);
+		    }
+	    	sprintf(stat, "%s\n", send_stat_cal(timer.ElapseduSec(), i));
+	    }
+	}
 }
 
 void udp_server(){
